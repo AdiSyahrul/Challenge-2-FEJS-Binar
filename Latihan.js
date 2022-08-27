33851a3ff61099c5a2538452e287b0fa36dcac0c
AdiSyahrul <adisyahrul27@gmail.com>
2022-08-27 11:32:56 +0700

    update soal 7

--- Latihan.js
+++ Latihan.js
@@ -158,5 +158,78 @@ console.log(getAngkaTerbesarKedua(dataNumbers));
 console.log(getAngkaTerbesarKedua(0));
 console.log(getAngkaTerbesarKedua());
 
-
 //END Soal 6
+
+//START Soal 7
+console.log('\n=============SOAL 7=============')
+const dataPenjualanPakAldi = [
+    {
+        namaProduct : 'Sepatu Futsal Nike Vapor Academy 8',
+        hargaSatuan : 760000,
+        kategori : "Sepatu Sport",
+        totalTerjual : 90,
+    },
+    {
+        namaProduct : 'Sepatu Warrior Tristan Black Brown High',
+        hargaSatuan : 960000,
+        kategori : "Sepatu Sneaker",
+        totalTerjual : 37,
+    },
+    {
+        namaProduct : 'Sepatu Warrior Tristan Maroon High',
+        hargaSatuan : 360000,
+        kategori : "Sepatu Sneaker",
+        totalTerjual : 90,
+    },
+    {
+        namaProduct : 'Sepatu Warrior Ranbow Tosca Corduroy',
+        hargaSatuan : 120000,
+        kategori : "Sepatu Sneaker",
+        totalTerjual : 90,
+    }
+]
+console.log(dataPenjualanPakAldi.reduce((n, {totalTerjual}) => n + totalTerjual, 0))
+//END Soal 7
+
+//START Soal 8
+console.log('\n=============SOAL 8=============')
+const dataPenjualanNovel = [
+    {
+      idProduct: 'BOOK002421',
+      namaProduk: 'Pulang - Pergi',
+      penulis: 'Tere Liye',
+      hargaBeli: 60000,
+      hargaJual: 86000,
+      totalTerjual: 150,
+      sisaStok: 17,
+    },
+    {
+      idProduct: 'BOOK002351',
+      namaProduk: 'Selamat Tinggal',
+      penulis: 'Tere Liye',
+      hargaBeli: 75000,
+      hargaJual: 103000,
+      totalTerjual: 171,
+      sisaStok: 20,
+    },
+    {
+      idProduct: 'BOOK002941',
+      namaProduk: 'Garis Waktu',
+      penulis: 'Fiersa Besari',
+      hargaBeli: 67000,
+      hargaJual: 99000,
+      totalTerjual: 213,
+      sisaStok: 5,
+    },
+    {
+      idProduct: 'BOOK002941',
+      namaProduk: 'Laskar Pelangi',
+      penulis: 'Andrea Hirata',
+      hargaBeli: 55000,
+      hargaJual: 68000,
+      totalTerjual: 20,
+      sisaStok: 56,
+    },
+];
+
+//END Soal 8
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,8 @@
-const dataAngka=[9,4,7,7,4,3,2,2,8];
+      
+//       dataPenjualanPakAldi  .map(hitungTotalPenjualan).reduce(sum);
+//       // => 235;
+      
+//       // or use arrow functions
+//       traveler.map(item => item.Amount).reduce((prev, next) => prev + next);
+//     return dataPenjualan.hitungTotalPenjualan;
+// dataPenjualanPakAldi.filter(hitungTotalPenjualan);
